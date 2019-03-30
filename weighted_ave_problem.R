79178f8e37430e63abefe7bf723343b2438ce1ce
Sebastian Carroll <sebastianjamescarroll@gmail.com>
2019-03-30 13:13:46 +0000

    Almost there
    Numbers look better but still not correct. The population looks ok with india forming 40%ish for example. The freq averages don't sum to 100 though so something still wrong there

--- weighted_ave_problem.R
+++ weighted_ave_problem.R
@@ -1,39 +1,38 @@
+# added the packages
 install.packages("dplyr")    # alternative installation of the %>%
 install.packages("ggplot2")
   
 library(ggplot2)
 library(dplyr)
 
-#region - dear god seb please help me
-#i need averages of each answer (home_w_fam_v_outside_w_friends) per region (south asia etc) 
-#as a weighted averages by population of each country (moldova == 2%, Russia == 10% etc)
-#....it is killing me. This is what I have got.
-
 #read dataframe and save as wdye
 wdye <- read.csv('where_do_you_eat.csv')
 
-
-# group it by country then create a variable called freq_perc which is the percentage of each answer by country 
-#(sum of all answers/ country = 100)
-country_group <- group_by(wdye, country)
-wdye_with_freq_decimal <- dplyr::mutate(country_group, 
-                                        freq_deci = freq/sum(freq), 
-                                        freq_perc = freq_deci*100,
-                                        pop_deci = population/sum(population),
-                                        pop_freq = pop_deci*100
-                                        )
+# Think we want to use the total surveyed population not the sum(population) as that will have duplicates
+# Not sure how to do this within dplyr
+total_pop <- wdye %>% distinct(population)
+sum_total_pop <- sum(total_pop)
 
 wdye_weight <- wdye %>%
   mutate(
-    pop_deci = population/sum(population),
+    # Take the popluation before grouping
+    pop_deci = population/sum_total_pop,
     pop_percent = pop_deci*100
   ) %>%
   group_by(country) %>%
   mutate(
     freq_deci = freq/sum(freq), 
     freq_percent = freq_deci*100
-  ) 
-  
+  )
+
+wdye_aves <- wdye_weight %>% 
+  group_by(home_w_fam_v_outside_w_friends) %>% 
+  mutate(freq_aves = weighted.mean(x = freq_deci, w = pop_deci)*100)
+
+# freq_aves seems off
+# Doesn't sum to 100
+ggplot(wdye_aves, aes(region, freq_aves)) + geom_col()
+
 
 #group by country and determine the weight by country population as a percentage of total population
 #FOR SOME FUCKING REASON SO MANY ARE THE SAME NUMBER AND I CAN"T FIX IT
@@ -42,7 +41,15 @@ wdye_weight <- wdye %>%
 ##I then use freq_perc as the scores and weights as the weights to get weighted means. These should be different 
 #values for each country that I then average together that give me average rates for each answer by region.
 #I should have one score for each answer, instead I have thi garbage...fuck
-wdye_aves <- wdye_weight %>% dplyr::group_by(home_w_fam_v_outside_w_friends) %>% 
-  dplyr::mutate(freq_aves = weighted.mean(x = freq_perc, w = weights)*100)
 
-ggplot(wdye_aves, aes(region, freq_aves)) + geom_col()
+# wtf is a weighted mean?
+# google to the rescue
+wt <- c(5,  5,  4,  1)/15
+x <- c(3.7,3.3,3.5,2.8)
+xm <- weighted.mean(x, wt)
+# > 3.45333
+# By hand:
+3.7*5/15 + 3.3*5/15 + 3.5*4/15 + 2.8*1/15
+# = 3.45333
+
+
