970d61a47d56d508a35bd027af0bdc0f39a469ce
Sebastian Carroll <sebastianjamescarroll@gmail.com>
2019-03-30 11:39:05 +0000

    Initial investigation
    No idea what the pipline operator does. Tried to rewrite it using standard assigment

--- weighted_ave_problem.R
+++ weighted_ave_problem.R
@@ -1,4 +1,8 @@
-
+install.packages("dplyr")    # alternative installation of the %>%
+install.packages("ggplot2")
+  
+library(ggplot2)
+library(dplyr)
 
 #region - dear god seb please help me
 #i need averages of each answer (home_w_fam_v_outside_w_friends) per region (south asia etc) 
@@ -11,7 +15,14 @@ wdye <- read.csv('where_do_you_eat.csv')
 
 # group it by country then create a variable called freq_perc which is the percentage of each answer by country 
 #(sum of all answers/ country = 100)
-wdye_weight <- wdye %>% group_by(country) %>% dplyr::mutate(freq_perc = freq/sum(freq)*100)
+country_group <- group_by(wdye, country)
+wdye_with_freq_decimal <- dplyr::mutate(country_group, freq_deci = freq/sum(freq))
+wdye_with_freq_weight <- dplyr::mutate(wdye_with_freq_decimal, freq_perc = freq_deci*100)
+with_pop_weight <- dplyr::mutate(wdye_with_freq_weight, population_weight = ((population/sum(population))*100))
+
+wdye_weight <- wdye %>% 
+  group_by(country) %>%
+  dplyr::mutate(freq_perc = freq/sum(freq)*100)
 
 #group by country and determine the weight by country population as a percentage of total population
 #FOR SOME FUCKING REASON SO MANY ARE THE SAME NUMBER AND I CAN"T FIX IT
@@ -22,9 +33,5 @@ wdye_weight <- wdye_weight %>% dplyr::group_by(country) %>% dplyr::mutate(weight
 #I should have one score for each answer, instead I have thi garbage...fuck
 wdye_aves <- wdye_weight %>% dplyr::group_by(home_w_fam_v_outside_w_friends) %>% 
   dplyr::mutate(freq_aves = weighted.mean(x = freq_perc, w = weights)*100)
-
-
-
-
 
 ggplot(wdye_aves, aes(region, freq_aves)) + geom_col()
